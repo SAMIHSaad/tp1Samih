f7a04d9777dc4a472f6e66a8f652ca291266c2e8
SaadSAMIH <saadsamih23@gmail.com>
2025-10-29 10:11:57 +0100

    execution de l'application

--- src/main/java/ma/emsi/samih/tp1samih/llm/JsonUtilPourGemini.java
+++ src/main/java/ma/emsi/samih/tp1samih/llm/JsonUtilPourGemini.java
@@ -136,7 +136,7 @@ public class JsonUtilPourGemini implements Serializable {
                         .add("text", question))
                 .build();
         JsonObject userContent = Json.createObjectBuilder()
-                .add("role", "user")
+                .add("role", "system")
                 .add("parts", userContentParts)
                 .build();
         JsonArray contents = Json.createArrayBuilder()
@@ -215,3 +215,5 @@ public class JsonUtilPourGemini implements Serializable {
     }
 
 }
+
+//Problème de connexion avec l'API du LLM400 : Bad Request
